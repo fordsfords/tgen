2ee2b8fa226420d467b261cbe4b0f5291144e117
fordsfords <sford@geeky-boy.com>
2023-07-10 08:55:06 -0400

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -13,6 +13,7 @@ if [ $? -ne 0 ]; then echo error in tgen.c; exit 1; fi
 # Update doc table of contents (see https://github.com/fordsfords/mdtoc).
 if which mdtoc.pl >/dev/null; then mdtoc.pl -b "" README.md;
 elif [ -x ../mdtoc/mdtoc.pl ]; then ../mdtoc/mdtoc.pl -b "" README.md;
+else echo "FYI: mdtoc.pl not found; see https://github.com/fordsfords/mdtoc"
 fi
 
 
